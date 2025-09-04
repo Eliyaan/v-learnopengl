d17bc7a2fe5458fd31deee2218841068743c121b
Eliyaan <nopana1007@gmail.com>
2025-09-04 15:58:13 +0200

    fmt

--- hellowindow/main.v
+++ hellowindow/main.v
@@ -10,20 +10,20 @@ mut:
 fn main() {
 	mut app := &App{}
 	sapp.run(
-		width: 800
-		height: 600
-		user_data: app
-		init_userdata_cb: init
+		width:             800
+		height:            600
+		user_data:         app
+		init_userdata_cb:  init
 		frame_userdata_cb: frame
 		event_userdata_cb: event
-		window_title: c'HelloWindow'
+		window_title:      c'HelloWindow'
 	)
 }
 
 fn init(mut app App) {
 	mut desc := sapp.create_desc()
 	gfx.setup(&desc)
-//	app.pass_action = gfx.create_clear_pass_action(0.2, 0.3, 0.3, 1.0)
+	//	app.pass_action = gfx.create_clear_pass_action(0.2, 0.3, 0.3, 1.0)
 	app.pass_action.colors[0] = gfx.ColorAttachmentAction{
 		load_action: .clear
 		clear_value: gfx.Color{0.2, 0.3, 0.3, 1.0}
@@ -39,9 +39,9 @@ fn event(mut e sapp.Event, mut app App) {
 }
 
 fn frame(mut app App) {
-//	pass := sapp.create_default_pass(app.pass_action)
+	//	pass := sapp.create_default_pass(app.pass_action)
 	pass := &gfx.Pass{
-		action: app.pass_action
+		action:    app.pass_action
 		swapchain: sapp.glue_swapchain()
 	}
 	gfx.begin_pass(pass)
